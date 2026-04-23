faf0483ebb6611633104b88d6a4a09ae36c3fa4a
Dmitry Volkov <dmitry@MacBook-Pro-3.local>
2026-04-23 10:41:17 +0300

    chore: remove old landing components and supabase dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import Hero from './components/Hero';
-import ForYou from './components/ForYou';
-import ValueConnection from './components/ValueConnection';
-import HowItWorks from './components/HowItWorks';
-import ThreeServices from './components/ThreeServices';
-import Advantages from './components/Advantages';
-import FAQ from './components/FAQ';
-import CTA from './components/CTA';
-import Footer from './components/Footer';
-
 function App() {
-  return (
-    <div className="min-h-screen">
-      <Hero />
-      <ForYou />
-      <ValueConnection />
-      <HowItWorks />
-      <ThreeServices />
-      <Advantages />
-      <FAQ />
-      <CTA />
-      <Footer />
-    </div>
-  );
+  return <div className="min-h-screen" />;
 }
 
 export default App;
